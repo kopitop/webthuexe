5872569c015e22a7ee74ce5a083fb412dcfd401a
HiepNM <reds99@AppleHouses-MacBook-Pro-3.local>
2017-05-13 14:03:15 +0700

    fix https

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -12,7 +12,7 @@
         <link href="{{ asset('css/all.css') }}" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <link href='http://fonts.googleapis.com/css?family=Playball' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Playball' rel='stylesheet' type='text/css'>
     </head>
     <body>
         <div class="header-bg">
